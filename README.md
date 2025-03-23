957053c4248b8592f9d6daff1db3c6edff5a011a
Tenorio Rojas <etenorio@indracompany.com>
2025-03-22 21:58:16 -0500

    feature:documentation and readme

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# Coding Challenge
+# Basic API 🔥
+## SECTION
+1. Description
+    
+
+2. 🗂️Folder Structure
+    - 📁 templates
+        - index.html
+    - 📁 uploads
+    - 🐍 app.py 
+
+
+3. 📚 Requirements
+    - 🐍 Python 3.11.4
+    -  Flask 2.2.5
+4. Install requirements
+    ```
+    pip install -r requirements.txt
+    ```
--- app.py
+++ app.py
@@ -3,8 +3,10 @@ from flask import Flask, render_template, request
 from werkzeug.utils import secure_filename
 import os
 
+# Variable Section
 app = Flask(__name__)
 
+# Check id the folder exist
 os.makedirs('uploads/', exist_ok=True)
 
 @app.route('/')
@@ -13,13 +15,19 @@ def home():
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
+    # Check if file comes with teh request
     if 'file' in request.files:
+        # Catch file
         file = request.files['file']
+        # Secure the name of the file
         filename = secure_filename(file.filename)
+        # Save the file in the folder that was checked before
         file.save(f'uploads/{filename}')
         return 'File uploaded successfully'
 
+    # Returns if the file has not been uploaded
     return 'No file uploaded'
 
+# call to the program 
 if __name__ == '__main__':
     app.run()
